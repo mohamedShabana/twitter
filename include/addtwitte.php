ab955a3ed00d1431ef3c5aa9bc407410641f92a4
Mohamed Monir shabana <mohamed_ms2006@hotmail.com>
2023-05-05 15:44:18 +0300

    add new twitte for add post to DB

---
+++ include/addtwitte.php
@@ -0,0 +1,5 @@
+<form action="twitte.php" method="POST" class="">
+                <textarea name="artic" class="form-control mb-2">
+                </textarea>
+                <button type="submit" class="btn btn-primary">Submit</button>
+</form>
---
+++ include/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <script src="./js/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
