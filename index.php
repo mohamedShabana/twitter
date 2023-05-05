8fe53a69346e4e414a6f5b829639a924539dfe21
Mohamed Monir shabana <mohamed_ms2006@hotmail.com>
2023-05-05 14:50:46 +0300

    add Framework bootstrap

--- index.php
+++ index.php
@@ -1,4 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/botstrap.min.css">
+</head>
+<body>
 <?php 
     $name = "Ali";
     echo "<h1> Hi ".$name." </h1>";
 ?>
+<footer>
+    <script src="./js/bootstrap.min.js"></script>
+</footer>
+</body>
+</html>
+
